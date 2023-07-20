3e5460b1c017df8db3315c5596ac3836025b026b
Ildar <ildar.riki@mail.ru>
2023-07-20 14:08:29 +0400

    10 мин и готово

---
+++ Project_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} чётное число ");
+} else {
+    Console.WriteLine($"{num} не чётное число ");
+}
---
+++ Project_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"N: {i}");
+    }   
+    i++;
+}
